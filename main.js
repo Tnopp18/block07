ff2a6e01df8ba456e0bb0ab279b94324d93413d5
Thomas Noppenberger <thomas.lee.noppenberger@gmail.com>
2024-03-04 14:21:02 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+/*Psuedo Code
+1.Create index.html file
+2.create main.js file
+3.link java in the html
+4.Create variables for the code 
+5.make a string for message
+6.add an alert tag
+7. combine string and variables in alert tag
+8.check webpage to see is alert pops up 
+9.submite to github
+10.Submit assignment
+*/
+
+const string1 = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`
+//should be first in alert tag
+
+const num1 = 50 - 40;
+console.log(num1);
+//expected outcome 10
+
+const num2 = 4 * 10;
+console.log(num2);
+//expected outcome 40
+
+const num3 = 30 + 9;
+console.log(num3);
+//expected outcome 39
+
+alert(`${string1} ${num1} ${num2} ${num3}`)
